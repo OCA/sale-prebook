OCA/sale-workflow

[ADD] sale_stock_prebook_cancel_line: Glue addon
This addon is a technical module that ensures that the computation of the cancelled quantity on a sale order line is correct even when a prebooking is involved. Cancelled prebooked quantities must not be taken into account in the computation of the cancelled quantity on the sale order line.

sale_stock_prebook_cancel_line/__manifest__.py
@@ -1,0 +1,19 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Stock Prebook Cancel Line",
+    "summary": """Takes into account prebook pickings into the computation
+     of cancelled qty on the sale order lines""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": [
+        "sale_stock_prebook",
+        "sale_order_line_cancel",
+    ],
+    "auto_install": True,
+    "data": [],
+    "demo": [],
+}

OCA/sale-prebook

sale_stock_prebook_cancel_line/__manifest__.py
@@ -1,0 +1,19 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Stock Prebook Cancel Line",
+    "summary": """Takes into account prebook pickings into the computation
+     of cancelled qty on the sale order lines""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-prebook",
+    "depends": [
+        "sale_stock_prebook",
+        "sale_order_line_cancel",
+    ],
+    "auto_install": True,
+    "data": [],
+    "demo": [],
+}